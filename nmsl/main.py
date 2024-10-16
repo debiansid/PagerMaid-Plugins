fd0702d8619ca41b14690ecd130e93f0b8b3270f
ミドリ <midori01@users.noreply.github.com>
2024-10-16 19:25:54 +0900

    Update main.py

--- nmsl/main.py
+++ nmsl/main.py
@@ -1,47 +1,38 @@
-from asyncio import sleep
-from pyrogram.errors import ChatAdminRequired, FloodWait
-from pagermaid.dependence import add_delete_message_job
+from pyrogram.errors import ChatAdminRequired
 from pagermaid.listener import listener
 from pagermaid.services import bot
 from pagermaid.enums import Message
-from pagermaid.utils import lang
 
 
-async def ban_user(chat_id: int, user_id: int):
+async def ban_user_and_notify(chat_id: int, user_id: int, message: Message, user_mention: str, chat_title: str):
     await bot.ban_chat_member(chat_id, user_id)
     await bot.delete_user_history(chat_id, user_id)
+    text = f'用户 {user_mention} (`{user_id}`) 已在群组 {chat_title} (`{chat_id}`) 被封禁。'
+    await message.edit(text)
 
 
 @listener(
     command="nmsl",
-    description="ban user",
+    description="",
     need_admin=True,
     groups_only=True,
 )
 async def super_ban(message: Message):
-    chat = message.chat
     reply = message.reply_to_message
 
     if not reply or not reply.from_user:
-        await message.edit(lang("arg_error"))  # 必须回复目标消息
-        return add_delete_message_job(message, 10)
+        await message.edit("必须回复目标消息")
+        return await message.delete(10)
 
     uid = reply.from_user.id
     user_mention = reply.from_user.mention
-    chat_title = chat.title
+    chat_title = message.chat.title
 
     try:
-        await ban_user(chat.id, uid)
-        text = f'用户 {user_mention} (`{uid}`) 已在群组 {chat_title} (`{chat.id}`) 被封禁。'
-        await message.edit(text)
+        await ban_user_and_notify(message.chat.id, uid, message, user_mention, chat_title)
     except ChatAdminRequired:
-        await message.edit(lang("sb_no_per"))
-    except FloodWait as e:
-        await sleep(e.value)
-        await ban_user(chat.id, uid)
-        text = f'用户 {user_mention} (`{uid}`) 已在群组 {chat_title} (`{chat.id}`) 被封禁。'
-        await message.edit(text)
+        await message.edit("没有足够的权限封禁用户")
     except Exception as e:
         await message.edit(f"出现错误：{e}")
     finally:
-        add_delete_message_job(message, 10)
+        await message.delete(10)
