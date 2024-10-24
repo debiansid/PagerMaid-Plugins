891d9731021fbe9c03491744f1a184b465c0363b
ミドリ <midori01@users.noreply.github.com>
2024-10-25 00:06:24 +0900

    Update main.py

--- autochangenamex/main.py
+++ autochangenamex/main.py
@@ -1,61 +1,16 @@
-""" Module to automate message deletion. """
-
 import traceback
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timezone
 
 from pagermaid.dependence import scheduler
 from pagermaid.services import bot
-from pagermaid.utils import pip_install, logs
-
-pip_install("emoji")
-
-from emoji import emojize
-
-
-auto_change_name_init = False
-dizzy = emojize(":dizzy:", language="alias")
-cake = emojize(":cake:", language="alias")
-all_time_emoji_name = [
-    "clock12",
-    "clock1230",
-    "clock1",
-    "clock130",
-    "clock2",
-    "clock230",
-    "clock3",
-    "clock330",
-    "clock4",
-    "clock430",
-    "clock5",
-    "clock530",
-    "clock6",
-    "clock630",
-    "clock7",
-    "clock730",
-    "clock8",
-    "clock830",
-    "clock9",
-    "clock930",
-    "clock10",
-    "clock1030",
-    "clock11",
-    "clock1130",
-]
-time_emoji_symb = [emojize(f":{s}:", language="alias") for s in all_time_emoji_name]
+from pagermaid.utils import logs
 
 
 @scheduler.scheduled_job("cron", second=0, id="autochangename")
 async def change_name_auto():
     try:
-        time_cur = (
-            datetime.utcnow()
-            .replace(tzinfo=timezone.utc)
-            .astimezone(timezone(timedelta(hours=8)))
-            .strftime("%H:%M:%S:%p:%a")
-        )
-        hour, minu, seco, p, abbwn = time_cur.split(":")
-        shift = 1 if int(minu) > 30 else 0
-        hsym = time_emoji_symb[(int(hour) % 12) * 2 + shift]
+        local_time = datetime.now().strftime("%H:%M:%S")
+        hour, minu = local_time.split(":")[:2]
         _last_name = f"{hour}:{minu}"
         await bot.update_profile(last_name=_last_name)
         me = await bot.get_me()
