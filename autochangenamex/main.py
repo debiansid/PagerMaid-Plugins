d58eb5e7858b77a28cfbdb06d28e6509ab0b5dd0
ミドリ <midori01@users.noreply.github.com>
2024-10-25 01:24:09 +0900

    Update main.py

--- autochangenamex/main.py
+++ autochangenamex/main.py
@@ -1,9 +1,7 @@
-import traceback
-from datetime import datetime, timezone
+from datetime import datetime
 
 from pagermaid.dependence import scheduler
 from pagermaid.services import bot
-from pagermaid.utils import logs
 
 
 @scheduler.scheduled_job("cron", second=0, id="autochangename")
@@ -15,7 +13,6 @@ async def change_name_auto():
         await bot.update_profile(last_name=_last_name)
         me = await bot.get_me()
         if me.last_name != _last_name:
-            raise Exception("修改 last_name 失败")
-    except Exception as e:
-        trac = "\n".join(traceback.format_exception(e))
-        await logs.info(f"更新失败! \n{trac}")
+            pass
+    except Exception:
+        pass
