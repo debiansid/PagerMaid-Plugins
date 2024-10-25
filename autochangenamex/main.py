3b51fb9ec016920d4d8e81dca0d9fdcfd945f6d6
ミドリ <midori01@users.noreply.github.com>
2024-10-25 17:55:59 +0900

    Update main.py

--- autochangenamex/main.py
+++ autochangenamex/main.py
@@ -1,18 +1,19 @@
-from datetime import datetime
-
+from datetime import datetime, timedelta, timezone
 from pagermaid.dependence import scheduler
 from pagermaid.services import bot
-
 
 @scheduler.scheduled_job("cron", second=0, id="autochangename")
 async def change_name_auto():
     try:
-        local_time = datetime.now().strftime("%H:%M:%S")
-        hour, minu = local_time.split(":")[:2]
-        _last_name = f"{hour}:{minu}"
+        time_cur = datetime.utcnow().replace(tzinfo=timezone.utc).astimezone(
+            timezone(timedelta(hours=8))
+        ).strftime("%I:%M %p")
+        hour, minu_p = time_cur.split(":")
+        minu, p = minu_p.split()
+        _last_name = f"{hour}:{minu} {p}"
         await bot.update_profile(last_name=_last_name)
         me = await bot.get_me()
         if me.last_name != _last_name:
-            pass
+            raise Exception("修改 last_name 失败")
     except Exception:
         pass
